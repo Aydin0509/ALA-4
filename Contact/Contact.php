78db17d2ca1a4d2beda30f741750e3b320ca3b12
Aydin0509 <74608674+Aydin0509@users.noreply.github.com>
2021-04-02 17:20:14 +0200

    .

---
+++ Contact/Contact.php
@@ -0,0 +1,66 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="charge">
+  <meta name="keywords" content="project,energy,Charge,">
+  <meta name="author" content="Groep3">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="js/.js" type="text/javascript"></script>
+  <link href="CSS/style.css" rel="stylesheet" type="text/css"></link>
+  <link href="https://fonts.googleapis.com/css?family=Ropa+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <header>
+      <nav id="navbar">
+      <div class="topnav">
+        <a href="home.php" href="#home">Home</a>
+        <a href="Producten.php">Producten</a>
+        <a href="Evenementen.php">Evenementen</a>
+        <img class="logo" src="images/logo.png" height="5%"alt="">
+        <a href="FAQ.php">FAQ</a>
+        <a class="active">Contact</a>
+    </div>
+    </header>
+    <main>
+        
+        <img class="background" src="images/background.png" width="100%" height="100%" alt="Festival">
+        <h1 class="pagina">Contact<h1>
+        <div class="wrapper">
+            <div class="contact-form">
+                <div class="input-fields">
+                    <input type="text" class="input" placeholder="Vooraam">
+                    <input type="text" class="input" placeholder="Achternaam">
+                    <input type="text" class="input" placeholder="E-mail">
+                    <input type="text" class="input" placeholder="klacht">
+                    <input type="text" class="input" placeholder="vraag">
+                </div>
+                <div class="msg">
+                    <textarea placeholder="Extra informatie"></textarea>
+                    <div class="btn">Verstuur</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="box1">
+                <div class="icon"><i class="fa fa-user-o" aria-hidden="true"></i></div>
+                <div class="content">
+                    <h3>Contact</h3>
+                    <p><br>Charge@Energy.com<br><br>
+                       Tel: 010 245 5400</p>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
---
+++ Evenementen/evenementen.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="charge">
+  <meta name="keywords" content="project,energy,Charge,">
+  <meta name="author" content="Groep3">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="js/.js" type="text/javascript"></script>
+  <link href="CSS/evenementen.css" rel="stylesheet" type="text/css"></link>
+  <link href="https://fonts.googleapis.com/css?family=Ropa+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+      <nav id="navbar">
+      <div class="topnav">
+        <a href="home.php" href="#home">Home</a>
+        <a href="Producten.php">Producten</a>
+        <a class="active">Evenementen</a>
+        <img class="logo" src="images/logo.png" height="5%"alt="">
+        <a href="FAQ.php">FAQ</a>
+        <a href="Contact.php">Contact</a>
+    </div>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
